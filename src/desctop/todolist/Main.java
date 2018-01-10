fdfc6c5de017a45e58a09f071d3fa723d2af4a48
uladzimir.litvinchuk <litvinulad@gmail.com>
2018-01-10 16:32:52 +0300

    maven + log4j

--- src/desctop/todolist/Main.java
+++ src/desctop/todolist/Main.java
@@ -5,12 +5,15 @@ import desctop.todolist.logic.enums.Priority;
 import desctop.todolist.logic.enums.Status;
 import desctop.todolist.logic.utils.TaskConverter;
 import javafx.scene.control.Button;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Main{
-    Button button;
+    public static Logger log = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) {
         //int id, String text, String endDate, Priority priority, Status status
-        //System.out.println(TaskConverter.toCsvLineConvert(new Task(1, "text", "date", Priority.HIGH, Status.DONE)));
+        log.error(TaskConverter.toCsvLineConvert(new Task(1, "text", "date", Priority.HIGH, Status.DONE)));
         Task task = TaskConverter.toJavaConvert("1,texsdf sdf sdf SDFt,date,proir,status,fsdsd");
         System.out.println();
     }
