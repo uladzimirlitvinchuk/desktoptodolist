2a613d78ca5e318c0125e9649dccee1f09291986
uladzimir.litvinchuk <litvinulad@gmail.com>
2018-01-10 13:33:12 +0300

    toJava method

--- src/desctop/todolist/logic/utils/TaskConverter.java
+++ src/desctop/todolist/logic/utils/TaskConverter.java
@@ -1,10 +1,34 @@
 package desctop.todolist.logic.utils;
 
 import desctop.todolist.logic.beans.Task;
+import desctop.todolist.logic.enums.Priority;
+import desctop.todolist.logic.enums.Status;
 
 public class TaskConverter {
     public static Task toJavaConvert(String csvLine){
-        return null;
+        int id = 0;
+        String text = null;
+        String date = null;
+        Priority priority = null;
+        Status status = null;
+        try {
+            //1,texsdf sdf  260 sdf SDFt,23/11/2015,HIGH,DONE
+            String[] arr = csvLine.split(",", 5);
+            int lenght = arr.length;
+            id = Integer.parseInt(arr[0]);
+            text = arr[1];
+            if (lenght > 2) {
+                date = arr[2];
+            }if (lenght >3){
+                priority = Priority.valueOf(arr[3]);
+            }if (lenght == 5){
+                status = Status.valueOf(arr[4]);
+            }
+
+        }catch (Exception e){
+
+        }
+        return new Task(id,text,date,priority,status);
     }
     //int id, String text, String endDate, Priority priority, Status status
     public static String toCsvLineConvert (Task task){
